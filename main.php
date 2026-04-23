6ab093ee39c2e8db45a26633d0ac6a026304c8fd
jaomussu <joao.aro@edu.senai.br>
2026-04-23 11:54:13 -0300

    v.0.4 - Adicionando função de dividir.

--- main.php
+++ main.php
@@ -8,6 +8,10 @@ function Subtrair($a, $b){
     return $a - $b;
 }
 
-echo "Resultado: ", Subtrair(10,5);
+function DIvidir ($a, $b){
+    return $a / $b;
+}
+
+echo "Resultado: ", Dividir (10,5);
 
 ?>
