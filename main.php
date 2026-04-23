822f4817d6403f78b5a04eb84373760a4b8efab2
jaomussu <joao.aro@edu.senai.br>
2026-04-23 11:38:49 -0300

    v.0.2 - Adicionando funções.

--- main.php
+++ main.php
@@ -1,5 +1,9 @@
 <?php
 
-echo "Na aula do dia 23/04/2026 eu aprendi a mexer no GitHub/Git e desenvolver funções no PHP! =D";
+function Somar($a, $b, $c){
+    return $a + $b +$c;
+}
+
+echo "Resultado: ", Somar(5,7,30);
 
 ?>
