bd7c43f3ac9e4910d791c6f46df487e8f0d01f3a
jaomussu <joao.aro@edu.senai.br>
2026-04-28 09:30:47 -0300

    v.0.7 - Adicionando função conversão.

--- entradaDados.php
+++ entradaDados.php
@@ -8,15 +8,14 @@ use function conversor\pesoParaReal;
 use function conversor\libraParaReal;
 use function conversor\ieneParaReal;
 
-echo "Dolar para Real: ", dolarParaReal(5, 5.00); 
-"\n";
-echo "Euro para Real: ", euroParaReal(10, 5.84); 
-"\n";
-echo "Peso para Real: ", pesoParaReal(300, 0.0035); 
-"\n";
-echo "Libra para Real: ", libraParaReal(20, 6.71); 
-"\n";
-echo "Iene para Real: ", ieneParaReal(100, 0.031); 
-"\n";
+echo "\nDolar para Real: ", dolarParaReal(5, 5.00);
+
+echo "\nEuro para Real: ", euroParaReal(10, 5.84);
+
+echo "\nPeso para Real: ", pesoParaReal(300, 0.0035);
+
+echo "\nLibra para Real: ", libraParaReal(20, 6.71);
+
+echo "\nIene para Real: ", ieneParaReal(100, 0.031);
 
 ?>
